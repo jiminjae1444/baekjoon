685571cf09efb3a19a555980cf941323b6d9d98f
jiminjae1444 <81136721+jiminjae1444@users.noreply.github.com>
2024-12-20 12:19:43 +0900

    [level 2] Title: ROOT 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/273710. ROOT 아이템 구하기/ROOT 아이템 구하기.sql
@@ -0,0 +1,2 @@
+-- 코드를 작성해주세요
+select item_info.item_id,item_name from item_info join item_tree on item_info.item_id = item_tree.item_id where item_tree.PARENT_ITEM_ID is null order by item_info.item_id;
